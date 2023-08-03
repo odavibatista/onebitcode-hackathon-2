0cf8e846e243a6f918ebe912a698cfca790f48cf
Davi Batista <davideosmar11@gmail.com>
2023-08-03 14:29:45 -0300

    feat: fixed some responsivity problems, added 'terms' and 'privacy politics' section

--- src/app/views/privacy/privacy.component.ts
+++ src/app/views/privacy/privacy.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core'
+import { Component, Output } from '@angular/core'
 
 @Component({
     selector: 'app-privacy',
     templateUrl: './privacy.component.html',
     styleUrls: ['./privacy.component.scss']
 })
-export class PrivacyComponent {}
+export class PrivacyComponent {
+    @Output() politics = [
+        {
+            title: 'Informações Coletadas',
+            text: 'Nós coletamos informações fornecidas por você, como nome, endereço, endereço de e-mail e informações de pagamento. Essas informações são protegidas e usadas apenas para fins de cobrança. Além disso, coletamos informações técnicas, como o seu endereço IP, o tipo de navegador que você usa, o endereço web de referência e sites visitados, e qualquer informação fornecida em suas solicitações, serviços ou comentários.'
+        },
+
+        {
+            title: 'Uso de Informações',
+            text: 'Nossos serviços incluem usar as informações que coletamos para fins de cobrança ou para responder a pedidos, perguntas e reivindicações. Podemos compartilhar suas informações com nossos provedores de serviço para garantir o bom funcionamento do nosso sistema e que os serviços sejam oferecidos de maneira eficaz.'
+        },
+
+        {
+            title: 'Segurança',
+            text: 'Tomamos medidas para manter a confidencialidade e segurança das informações que você envia. Não armazenamos informações pessoais, exceto nos casos em que queremos oferecer recursos específicos ou serviços solicitados por nossos clientes.'
+        }
+    ]
+}
--- src/app/views/terms/terms.component.ts
+++ src/app/views/terms/terms.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core'
+import { Component, Output } from '@angular/core'
 
 @Component({
     selector: 'app-terms',
     templateUrl: './terms.component.html',
     styleUrls: ['./terms.component.scss']
 })
-export class TermsComponent {}
+export class TermsComponent {
+    @Output()   terms   =   [
+        'Para se criar uma conta, é necessário ter 18 anos ou mais. Por isso, para acessar o website e realizar alterações nos dados, os usuários deverão confirmar sua maioridade e se responsabilizar por fornecer informações verdadeiras.',
+        'Os preços e descontos oferecidos pelo estabelecimento estão sujeitos a alterações a qualquer momento.',
+        'Os usuários autorizam a empresa a utilizar seus dados pessoais para envio de promoções, ofertas e outros fins.',
+        'A empresa se reserva o direito de alterar os termos de uso a qualquer momento.',
+        'Os usuários se obrigam a fornecer seus dados reais. Havendo falseabilidade ou irregularidade dos mesmos, a conta será suspensa e seus dados banidos',
+        'Em caso de banimento ou falha, é possível solicitar recurso via atendimento online, via ticket por e-mail (suporte@xhealth.com)',
+        'É expressamente proibido cadastrar informações de terceiros neste site sem sua devida tutela ou autorização. Em caso de imputação de dados de terceiros para compras no nome destes, as autoridades digitais serão acionadas.',
+        'O usuário consente nas estipulações e distinções de cada um dos métodos de pagamento, estando sujeito às suas particularidades e tratos.',
+        'As artes autorais não podem ser utilizadas comercialmente em outros sítios ou domínios da internet, sob pena de intimação judicial. Caso seja para divulgação, críticas ou notícias, este ato é permitido e aceito.'
+    ]
+}
